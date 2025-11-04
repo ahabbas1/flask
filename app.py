a8352c637172d8eb793e89b65f22cb93a082cb14
Ahmed Abbas <ahmedabbas2004@gmail.com>
2025-11-03 21:44:52 -0600

    final

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template('index.html') 
+
 @app.route('/about')
-
-
+def about_page():
+    return render_template('about.html')
 
 if __name__ == "__main__":
       app.run(host="0.0.0.0", port=5002, debug=True)
